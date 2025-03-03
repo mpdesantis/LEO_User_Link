6b8f4a068adb5d7711e839a17cb5f942d29e5266
Michael De Santis <michaelpeterdesantis@gmail.com>
2025-03-02 21:15:51 -0500

    clean up main

--- main/main.cpp
+++ main/main.cpp
@@ -1,68 +1,32 @@
 #include <limits>
 #include "include/top.hpp"
-
-/*
-There are 3 macros defined at compile time that changes the behaviour of the simulation.
---> SIM_TIME: This macro, when defined, runs the simulation in simulation time. Else, the simulation runs at wall clock.
---> ESP_PLATFORM: When defined, the models are compiled for the ESP32 microcontroller. Else, compiles for Linux/ Windows
---> NO_LOGGING: When defined, prevents logging (maybe useful in embedded situations)
-*/
-
-
-#ifdef SIM_TIME
-	#include "cadmium/simulation/root_coordinator.hpp"
-#else
-	#include "cadmium/simulation/rt_root_coordinator.hpp"
-	#ifdef ESP_PLATFORM
-		#include <cadmium/simulation/rt_clock/ESPclock.hpp>
-	#else
-		#include <cadmium/simulation/rt_clock/chrono.hpp>
-	#endif
-#endif
-
-#ifndef NO_LOGGING
-	#include "cadmium/simulation/logger/stdout.hpp"
-	#include "cadmium/simulation/logger/csv.hpp"
-#endif
+#include "cadmium/simulation/root_coordinator.hpp"
+#include "cadmium/simulation/logger/stdout.hpp"
+#include "cadmium/simulation/logger/csv.hpp"
 
 using namespace cadmium;
 
-extern "C" {
-	#ifdef ESP_PLATFORM
-		void app_main() //starting point for ESP32 code
-	#else
-		int main()		//starting point for simulation code
-	#endif
-	{
-	
-		auto model = std::make_shared<top_coupled> ("top");
-		
-		#ifdef SIM_TIME
-			auto rootCoordinator = cadmium::RootCoordinator(model);
-		#else
-			#ifdef ESP_PLATFORM
-				cadmium::ESPclock clock;
-				auto rootCoordinator = cadmium::RealTimeRootCoordinator<cadmium::ESPclock<double>>(model, clock);
-			#else
-				cadmium::ChronoClock clock;
-				auto rootCoordinator = cadmium::RealTimeRootCoordinator<cadmium::ChronoClock<std::chrono::steady_clock>>(model, clock);
-			#endif
-		#endif
+int main() {
 
-		#ifndef NO_LOGGING
-		rootCoordinator.setLogger<STDOUTLogger>(";");
-		#endif
+    // Top model
+	auto model = std::make_shared<top_coupled> ("top");
 
-		rootCoordinator.start();
-		#ifdef ESP_PLATFORM
-			rootCoordinator.simulate(std::numeric_limits<double>::infinity());
-		#else
-			rootCoordinator.simulate(23.0);
-		#endif
-		rootCoordinator.stop();	
+    // Root coordinator
+	auto rootCoordinator = RootCoordinator(model);
 
-		#ifndef ESP_PLATFORM
-			return 0;
-		#endif
-	}
+    // `stdout` logging
+	// rootCoordinator.setLogger<STDOUTLogger>(";");
+
+    // CSV file logging
+	rootCoordinator.setLogger<CSVLogger>("top_model_output.csv", ";");
+
+    // Start the root coordinator
+	rootCoordinator.start();
+    // Begin simulation
+	rootCoordinator.simulate(10.1);
+    // Stop simulation
+	rootCoordinator.stop();	
+
+	return 0;
 }
+
