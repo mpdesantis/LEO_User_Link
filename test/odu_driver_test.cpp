5b0d264a3b133550c7e276df7076b5dca8f5fc7c
Michael De Santis <michaelpeterdesantis@gmail.com>
2025-03-07 13:01:21 -0500

    test directory initialized and build rules defined

---
+++ test/odu_driver_test.cpp
@@ -0,0 +1,42 @@
+// std
+#include <limits>
+
+// Models
+#include "top_odu.hpp"
+
+// Cadmium V2
+#include "cadmium/simulation/root_coordinator.hpp"
+#include "cadmium/simulation/logger/stdout.hpp"
+#include "cadmium/simulation/logger/csv.hpp"
+
+using namespace cadmium;
+
+int main() {
+
+    /**
+     * TEST: ODU
+     */
+
+    // Top model
+	auto model = std::make_shared<TopCoupledOduTest> ("TOP");
+    double simulation_duration = 20.00;
+
+    // Root coordinator
+	auto rootCoordinator = RootCoordinator(model);
+
+    // `stdout` logging
+	//rootCoordinator.setLogger<STDOUTLogger>(";");
+
+    // CSV file logging
+	rootCoordinator.setLogger<CSVLogger>("output/top_coupled_odu_test.csv", ";");
+
+    // Start the root coordinator
+	rootCoordinator.start();
+    // Begin simulation
+	rootCoordinator.simulate(simulation_duration);
+    // Stop simulation
+	rootCoordinator.stop();	
+
+	return 0;
+}
+
