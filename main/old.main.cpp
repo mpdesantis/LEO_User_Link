2dcc4d43f16f22d1af37b454bc06247bd5c5fa47
Michael De Santis <michaelpeterdesantis@gmail.com>
2025-03-07 10:57:21 -0500

    backup main class

---
+++ main/old.main.cpp
@@ -0,0 +1,32 @@
+#include <limits>
+#include "include/top.hpp"
+#include "cadmium/simulation/root_coordinator.hpp"
+#include "cadmium/simulation/logger/stdout.hpp"
+#include "cadmium/simulation/logger/csv.hpp"
+
+using namespace cadmium;
+
+int main() {
+
+    // Top model
+	auto model = std::make_shared<top_coupled> ("top");
+
+    // Root coordinator
+	auto rootCoordinator = RootCoordinator(model);
+
+    // `stdout` logging
+	// rootCoordinator.setLogger<STDOUTLogger>(";");
+
+    // CSV file logging
+	rootCoordinator.setLogger<CSVLogger>("output/top_model_output.csv", ";");
+
+    // Start the root coordinator
+	rootCoordinator.start();
+    // Begin simulation
+	rootCoordinator.simulate(10.1);
+    // Stop simulation
+	rootCoordinator.stop();	
+
+	return 0;
+}
+
