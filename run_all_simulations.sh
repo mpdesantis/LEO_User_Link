69a11e0270969fe381f738ab17096e2c4f170161
Michael De Santis <michaelpeterdesantis@gmail.com>
2025-03-08 21:25:08 -0500

    update simulation execution script

--- run_all_simulations.sh
+++ run_all_simulations.sh
@@ -16,6 +16,7 @@
 # 
 # Notes:
 # ------
+# Ensure you have built the binaries with the `build_sim.sh` script before use.
 # Ensure this script has executable permission before use.
 #
 ###############################################################################
@@ -28,19 +29,21 @@ set -e
 SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname ${SCRIPT})
 
-echo -e "EXECUTING $SCRIPT..."
+echo -e "EXECUTING $SCRIPT...\n"
 
 # Run all simulations
 SIMULATION_DIR=${PWD}/bin
+echo -e "Executing all simulation binaries in ${SIMULATION_DIR} ...\n"
 for BINARY in "${SIMULATION_DIR}"/*;
 do
     if [[ -x "${BINARY}" && -f "${BINARY}" ]]; then
-        echo -e "Executing simulation binary: $(basename ${BINARY})"
+        echo -e "Executing simulation binary: bin/$(basename ${BINARY})"
+        echo -e "--> Corresponding output file: output/$(basename ${BINARY}).csv"
         "${BINARY}" 
     else
         echo "Skipping non-executable file: ${BINARY}"
     fi
 done
 
-echo -e "--- YOU EXECUTED ---"
+echo -e "\n--- YOU EXECUTED ---"
 
